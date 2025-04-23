b6459d45e10cb8ff38a071065b0991bf9ce37117
NithyaKrishnan08 <56358639+NithyaKrishnan08@users.noreply.github.com>
2025-04-23 16:22:48 -0400

    Initial commit

---
+++ index.ts
@@ -0,0 +1,13 @@
+type Person = {
+  name: string;
+  age: number;
+  email: string;
+}
+
+const person: Person = {
+  name: "John",
+  age: 30,
+  email: "john@example.com"
+}
+
+console.log(person.age);
